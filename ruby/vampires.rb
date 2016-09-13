af1c11a525162f63971ad5121d65c99119c77a88
Kelsey Edelstein <kelseyedelstein@gmail.com>
2016-09-13 15:34:16 -0700

    Release 1 - vampire interview questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+age = gets.chomp
+birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
+health_ins = gets.chomp 
