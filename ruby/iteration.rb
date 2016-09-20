73d8a06af9d99e0b721210fbc1b75d63b3cd4d04
Kelsey Edelstein <kelseyedelstein@gmail.com>
2016-09-20 12:47:34 -0700

    Add conditional iterative methods to hashes & arrays

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -26,3 +26,52 @@ p people_colors
 puts "People_colors hash after .each:"
 people_colors.each {|name, color| p name.capitalize}
 
+
+
+
+colors = ["blue", "green", "red", "orange"]
+
+people_colors = {
+	bob: "blue",
+	gary: "green",
+	roy: "red",
+	ollie: "orange"
+}
+
+
+colors.delete_if {|color| color.length < 5}
+p colors
+
+p people_colors.delete_if{|person, color| color == "red" }
+
+
+colors = ["blue", "green", "red", "orange"]
+
+people_colors = {
+	bob: "blue",
+	gary: "green",
+	roy: "red",
+	ollie: "orange"
+}
+
+colors.select! {|color| color.length < 5}
+p colors
+
+p people_colors.select!{|person, color| color == "orange" || color == "green"}
+
+colors = ["blue", "green", "red", "orange"]
+
+people_colors = {
+	bob: "blue",
+	gary: "green",
+	roy: "red",
+	ollie: "orange"
+}
+
+p colors.take_while {|color| color.index("r") == nil}
+
+p people_colors.keep_if {|person, color| person != :roy }
+
+colors = ["blue", "green", "red", "orange"]
+p colors.drop_while{|color| color.length < 5 }
+
