f1e7ae802fd2ced69e193feb073823d79645d9e5
Kelsey Edelstein <kelseyedelstein@gmail.com>
2016-09-26 11:14:44 -0700

    Add puppy class pseudocode

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+=begin 
+
+Puppy class
+
+SPECIES ----------------
+Canine
+
+CHARACTERISTICS --------
+Legs: 4
+Tail: 1
+Name: varies
+Color: varies
+Size: small
+
+BEHAVIOR ---------------
+Tail Wag
+Jump
+Bark
+Run
+Nap
+Fetch
+Roll Over
+
+
+=end
