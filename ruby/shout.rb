158c22df32f709fb654c39dd8d3014a9b7d8b8ef
Kelsey Edelstein <kelseyedelstein@gmail.com>
2016-09-27 17:19:14 -0700

    Create shout mixin and Student & Teacher classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,34 @@
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	#def self.yell_angrily(words)
+	#	words + "!!!" + " :("
+	#end
+	#def self.yelling_happily(words)
+	#	words + "!!! :)"
+	#end
+
+	def yell_angrily(words)
+		words.upcase + "!!! :("
 	end
-	def self.yelling_happily(words)
-		words + "!!! :)"
+
+	def yell_happily(words)
+		words.capitalize + "!!! :)"
 	end
+
 end
 
-Shout.yell_angrily("I'm angry") # => I'm angry!!! :(
-Shout.yelling_happily("I'm happy") # => I'm happy!!! :)
+#Shout.yell_angrily("I'm angry") 
+#Shout.yelling_happily("I'm happy") 
+
+class Student
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
+new_student = Student.new 
+p new_student.yell_angrily("what does this error message mean")
+
+new_teacher = Teacher.new
+p new_teacher.yell_happily("embrace confusion")
