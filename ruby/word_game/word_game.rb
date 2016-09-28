c2c2e05ac57d0ea750ce79bfebd7d5497b8554b8
Kelsey Edelstein <kelseyedelstein@gmail.com>
2016-09-28 15:25:35 -0700

    Create repeat & make_guess behaviors, add tests

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -1,3 +1,66 @@
-class WordGame
+=begin 
 
+WORD GUESSING GAME
+
+- variables to initialize:
+	- secret word (user input)
+	- guesses made = empty array
+	- guess limit = secret word length * 2
+	- game over = false 
+	- game won = false
+
+- accept a guess from the user 
+- IF not a repeated guess
+	- add to guesses made array
+	- IF guess exists in the secret word 
+		-reveal letter
+
+- check if game over OR game won
+- IF guesses made length >= guess limit
+	game over 
+- IF secret word revealed
+	game won
+
+=end
+
+class WordGame
+	attr_reader :secret_word
+	attr_accessor :guesses_made
+	def initialize(secret_word)
+		@secret_word = secret_word
+		@guesses_made = []
+		@current_display = []
+
+		@secret_word.split('').each do |letter|
+			@current_display << "_"
+		end
+	end
+
+	def repeat?(guess, guesses_made)
+		if @guesses_made.join('').index(guess)
+			true 
+		else
+			false
+		end
+	end
+
+	def make_guess(guess, guesses_made)
+		if !repeat?(guess, guesses_made)
+			@guesses_made << guess
+		end
+		@guesses_made
+	end
+
+	def display
+	end
 end
+
+# driver code 
+
+example_game = WordGame.new("secretword")
+
+p example_game.guesses_made
+example_game.make_guess("e",example_game.guesses_made)
+example_game.make_guess("f",example_game.guesses_made)
+p example_game.guesses_made
+p example_game.repeat?("f",example_game.guesses_made)
--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -1,8 +1,17 @@
 require_relative 'word_game'
 
 describe WordGame do 
-	let(:game) {WordGame.new}
+	let(:game) {WordGame.new("word")}
 	it "checks for a repeated guess" do 
-		expect(game.repeat?("a",["a","b"])).to eq true
+		guesses_made = game.make_guess("a",[])
+		expect(game.repeat?("a", guesses_made)).to eq true
+	end
+	it "adds a guess to the list of guesses made" do 
+		expect(game.make_guess("z",[])).to eq ["z"]
+	end
+	it "displays the current game state" do 
+		game.make_guess("r",[])
+		game.make_guess("a",["r"])
+		expect(game.display).to eq (["_","_","r","_"])
 	end
 end
