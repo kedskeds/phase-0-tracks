80d53eb3e8ce8af7be6465d3087f751e14b7461e
Kelsey Edelstein <kelseyedelstein@gmail.com>
2016-10-04 14:15:15 -0700

    Add code from pairing session

---
+++ js/data_structures.js
@@ -0,0 +1,12 @@
+var colors = ["blue","green","yellow","purple"];
+var names = ["ed","bob","george","kevin"];
+
+colors.push("orange");
+names.push("margie");
+
+
+var horses = {}
+
+for(var i = 0; i < colors.length; i++){
+	console.log(i);
+}
