48a19c5e6366159ffd96aac72957390c9e2edf32
Kelsey Edelstein <kelseyedelstein@gmail.com>
2016-10-04 19:57:01 -0700

    write function that creates random words

--- js/algos.js
+++ js/algos.js
@@ -24,8 +24,8 @@ longest = function(arr){
 }
 
 //examples
-console.log(longest(["long phrase","longest phrase","longer phrase"]));
-console.log(longest(["short","tiny","gigantic"]));
+//console.log(longest(["long phrase","longest phrase","longer phrase"]));
+//console.log(longest(["short","tiny","gigantic"]));
 
 /*
 
@@ -53,4 +53,53 @@ share_a_pair = function(obj1, obj2){
 }
 
 //examples
-console.log(share_a_pair({name: "Steven", age: 54},{name: "Tamir", age: 54}));
+//console.log(share_a_pair({name: "Steven", age: 54},{name: "Tamir", age: 54}));
+//console.log(share_a_pair({type: "car", make: "Honda"}, {type: "bicycle", brand: "Schwinn"}));
+
+/*
+
+random_words : int -> array of strings
+
+-for the given integer,
+	-initialize word_length (random # between 1 - 10)
+
+	-for the given word_length,
+		-generate a random letter of the alphabet
+		-add random letter to a given string
+	-push string to an array
+-return array
+
+*/
+
+random_words = function(int){
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	var len = alphabet.length;
+
+	var result = [];
+	for(var i = 0; i < int; i++){
+		word_length = Math.floor((Math.random()*10)+1);
+		str = "";
+		for(var j = 0; j < word_length; j++){
+			letter = Math.floor(Math.random()*len);
+			str += alphabet[letter];
+		}
+		result.push(str);
+	}
+	return result;
+}
+
+//examples
+//console.log(random_words(3));
+//console.log(random_words(5));
+
+for(var i = 0; i < 10; i++){
+	index = i + 1;
+	console.log("Result " + index.toString()+ ":")
+	console.log("");
+
+	arr = random_words(3);
+
+	console.log(arr);
+	console.log(longest(arr));
+	console.log("---------------------------");
+}
